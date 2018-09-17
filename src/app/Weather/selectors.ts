d4935f028fd85e69be6d01d47c9cc81a59aa9ea2
Edmond Atto <edmond091@gmail.com>
2018-09-17 18:51:42 +0300

    Add reselect, use createSelect to get main weather data

--- src/app/Weather/selectors.ts
+++ src/app/Weather/selectors.ts
@@ -1,9 +1,14 @@
+import { createSelector } from "reselect";
 import { State } from "../types";
 
-export const getMainWeatherData = (state: State) => {
-    const weatherData = state.weather.weatherData;
-    if (weatherData) {
-        return weatherData.weather[0];
+export const getWeatherData = (state: State) => state.weather.weatherData;
+
+export const getMainWeatherData = createSelector(
+    getWeatherData,
+    (weatherData) => {
+        if (weatherData) {
+            return weatherData.weather[0];
+        }
+        return;
     }
-    return;
-};
+);
